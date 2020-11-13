692115bc8ecd9c26d2ee25b5221b400819e8e25c
Unknown <dani_mrx93@hotmail.com>
2020-11-13 09:35:28 -0300

    Fix Player movement

--- GoEngineExe/Player.cpp
+++ GoEngineExe/Player.cpp
@@ -17,24 +17,24 @@ void Player::ManageCollision(vector<CollisionInfo> collisions) {
 void Player::Update(float deltaTime) {
 	velocity = Vector2().Zero();
 	if (Input::IsKeyPressed(Input::KEY_W)) {
-		velocity.y -= movementSpeed;
+		velocity.y -= 1;
 	}
 	if (Input::IsKeyPressed(Input::KEY_S)) {
-		velocity.y += movementSpeed;
+		velocity.y += 1;
 	}
 	if (Input::IsKeyPressed(Input::KEY_A)) {
-		velocity.x -= movementSpeed;
+		velocity.x -= 1;
 		animSprite->FlipVertical(true);
 	}
 	if (Input::IsKeyPressed(Input::KEY_D)) {
-		velocity.x += movementSpeed;
+		velocity.x += 1;
 		animSprite->FlipVertical(false);
 	}
 	if (Input::IsKeyPressed(Input::KEY_1)) {
 		animSprite->SetPosition(initialPos);
 	}
 	animSprite->Update(deltaTime);
-	animSprite->Translate(velocity * deltaTime);
+	animSprite->Translate(velocity.Normalize() * movementSpeed * deltaTime);
 }
 
 Player::Player() {
