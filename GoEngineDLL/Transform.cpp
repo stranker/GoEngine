27584dff6c7ccf0b67493728d829b0dcc50b0261
Unknown <dani_mrx93@hotmail.com>
2021-04-01 19:40:37 -0300

    Fix rotation and scale methods

--- GoEngineDLL/Transform.cpp
+++ GoEngineDLL/Transform.cpp
@@ -14,17 +14,21 @@ void Transform::SetPosition(Vector3 _position) {
 	UpdateModel();
 }
 
+void Transform::Translate(Vector3 _position){
+	SetPosition(position + _position);
+}
+
 void Transform::SetScale(float x, float y, float z) {
 	localScale.x = x;
 	localScale.y = y;
 	localScale.z = z;
-	matScale = glm::scale(glm::mat4(1.0f), glm::vec3(localScale.x , localScale.y , localScale.z));
+	matScale = glm::scale(matScale, glm::vec3(localScale.x , localScale.y , localScale.z));
 	UpdateModel();
 }
 
 void Transform::SetScale(Vector3 _scale) {
 	localScale = _scale;
-	matScale = glm::scale(glm::mat4(1.0f), glm::vec3(localScale.x, localScale.y, localScale.z));
+	matScale = glm::scale(matScale, glm::vec3(localScale.x, localScale.y, localScale.z));
 	UpdateModel();
 }
 
@@ -32,8 +36,39 @@ void Transform::SetRotation(float angle, Vector3 axis) {
 	rotation.x = angle * axis.x;
 	rotation.y = angle * axis.y;
 	rotation.z = angle * axis.z;
-	matRotation = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(axis.x, axis.y, axis.z));
+	matRotation = glm::rotate(matRotation, glm::radians(angle), glm::vec3(axis.x, axis.y, axis.z));
 	UpdateModel();
+}
+
+void Transform::RotateX(float angle) {
+	rotation.x = angle;
+	matRotation = glm::rotate(matRotation, glm::radians(angle), glm::vec3(1.0f, 0.0f, 0.0f));
+	UpdateModel();
+}
+
+void Transform::RotateY(float angle) {
+	rotation.y = angle;
+	matRotation = glm::rotate(matRotation, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+	UpdateModel();
+}
+
+void Transform::RotateZ(float angle) {
+	rotation.z = angle;
+	matRotation = glm::rotate(matRotation, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+	UpdateModel();
+}
+
+
+void Transform::LookAt(Vector3 _target) {
+	LookAt(position, _target, Vector3().Up());
+}
+
+void Transform::LookAt(Vector3 _target, Vector3 _upVector) {
+	LookAt(position, _target, _upVector);
+}
+
+void Transform::LookAt(Vector3 _position, Vector3 _target, Vector3 _upVector) {
+	transform = glm::lookAt(glm::vec3(_position.x, _position.y, _position.z), glm::vec3(_target.x, _target.y, _target.z), glm::vec3(_upVector.x, _upVector.y, _upVector.z));
 }
 
 Vector3 Transform::GetPosition() {
