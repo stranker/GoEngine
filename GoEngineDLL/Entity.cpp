0ac421ff690d657106d4801f9d6415c306e33817
Unknown <dani_mrx93@hotmail.com>
2021-04-01 18:20:44 -0300

    Rework Entity

--- GoEngineDLL/Entity.cpp
+++ GoEngineDLL/Entity.cpp
@@ -2,7 +2,56 @@
 #include "Renderer.h"
 #include "Transform.h"
 
+void Entity::CreateVertexArrayID() {
+	vertexArrayID = renderer->CreateVertexArrayID();
+}
+
+unsigned int Entity::GetVertexArrayID() const {
+	return vertexArrayID;
+}
+
+vector<Renderer::VertexData> Entity::GetVectorVertexData() const {
+	return vectorVertexData;
+}
+
+void Entity::CreateVertexData(float* _vertex, size_t dataSize, size_t vertexCount, Renderer::BufferType bufferType, size_t attributeID) {
+	Renderer::VertexData vertexData;
+	vertexData.dataCount = vertexCount;
+	vertexData.vbo = renderer->CreateVertexBuffer(_vertex, dataSize, bufferType);
+	vertexData.bufferType = bufferType;
+	vertexData.attributeID = attributeID;
+	vectorVertexData.push_back(vertexData);
+}
+
+void Entity::CreateVertexData(unsigned int* _vertex, size_t dataSize, size_t vertexCount, Renderer::BufferType bufferType, size_t attributeID) {
+	Renderer::VertexData vertexData;
+	vertexData.dataCount = vertexCount;
+	vertexData.vbo = renderer->CreateVertexBuffer(_vertex, dataSize, bufferType);
+	vertexData.bufferType = bufferType;
+	vertexData.attributeID = attributeID;
+	vectorVertexData.push_back(vertexData);
+}
+
+void Entity::UpdateVertexData(float* _vertex, size_t dataSize, size_t attributeID) {
+	// Posicion, Color, UV
+	for (Renderer::VertexData vertexData : vectorVertexData) {
+		if (vertexData.attributeID == attributeID) {
+			renderer->BindVertexArray(vertexArrayID); // Bindeo el VAO
+			vertexData.vbo = renderer->CreateVertexBuffer(_vertex, dataSize, vertexData.bufferType); // Update el VBO
+			renderer->BindVertexData(vertexData); // Bindeo el VBO
+			break;
+		}
+	}
+}
+
+void Entity::BindVertexObjects() {
+	for (Renderer::VertexData vertexData : vectorVertexData) {
+		renderer->BindVertexData(vertexData);
+	}
+}
+
 Entity::Entity(Renderer *_renderer) : renderer(_renderer) {
+	name = "Entity";
 	transform = new Transform();
 }
 
--- GoEngineDLL/Entity.h
+++ GoEngineDLL/Entity.h
@@ -1,13 +1,25 @@
+#pragma once
+
 #include "Exports.h"
 #include "Utils.h"
 #include "Transform.h"
 #include "Renderer.h"
 
-class ENGINEDLL_API Entity
-{
+class ENGINEDLL_API Entity{
 protected:
 	Transform *transform;
 	Renderer *renderer;
+
+	unsigned int vertexArrayID;
+	vector<Renderer::VertexData> vectorVertexData;
+	Renderer::Primitive primitive;
+	void CreateVertexArrayID();
+	unsigned int GetVertexArrayID() const;
+	vector<Renderer::VertexData> GetVectorVertexData() const;
+	void CreateVertexData(float* _vertex, size_t dataSize, size_t vertexCount, Renderer::BufferType bufferType, size_t attributeID);
+	void CreateVertexData(unsigned int* _vertex, size_t dataSize, size_t vertexCount, Renderer::BufferType bufferType, size_t attributeID);
+	void UpdateVertexData(float* _vertex, size_t dataSize, size_t attributeID);
+	void BindVertexObjects();
 public:
 	string name;
 	virtual void Draw() {};
