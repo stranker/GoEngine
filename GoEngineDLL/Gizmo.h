4b1269388145008cb126e4f5499792d264ee4e18
Unknown <dani_mrx93@hotmail.com>
2021-04-02 00:15:09 -0300

    Add Gizmo to visualize 3d objects

---
+++ GoEngineDLL/Gizmo.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Line3D.h"
+
+class ENGINEDLL_API Gizmo :
+    public Line3D {
+private:
+    Line3D* redLine;
+    Line3D* blueLine;
+    Line3D* greenLine;
+public:
+    void SetPosition(Vector3 position);
+    void Translate(Vector3 position);
+    void Rotate(Vector3  rotation);
+    void Rotate(float angle, Vector3 axis);
+    void Draw() override;
+    void Destroy() override;
+    Gizmo(Renderer* _renderer);
+    ~Gizmo();
+};
+
